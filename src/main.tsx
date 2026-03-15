139c6a13e5aae46348ccf73f0b563266ff582d01
Yevgeniy <ulchenkov2001@gmail.com>
2026-03-15 14:25:37 +0100

    [fix] workflow renamed

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,11 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { config, isDemo } from '@/config/runtime'
+import { config } from '@/config/runtime'
 import { routes } from '@/config/routes.ts'
 import { Dashboard } from '@/features/statistics'
 import { queryClient } from '@/config/queryClient.ts'
 import { QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export const AppRouter = () => {
   return (
@@ -26,7 +25,6 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <AppRouter />
-      {isDemo && <ReactQueryDevtools initialIsOpen={false} />}
     </QueryClientProvider>
   </StrictMode>,
 )
