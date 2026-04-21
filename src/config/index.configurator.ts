581e8780db4428b6a83603e4e928b221c0a1ed24
Yevgeniy <ulchenkov2001@gmail.com>
2026-04-21 11:46:41 +0200

    [fix] base path rewrite

--- src/config/index.configurator.ts
+++ src/config/index.configurator.ts
@@ -2,3 +2,7 @@ import { config } from '@/config/runtime'
 
 document.title = config.appTitle
 document.querySelector('meta[name="description"]')?.setAttribute('content', config.appTitle)
+
+if (window.location.pathname === '/' || !window.location.pathname.startsWith(config.basePath)) {
+  window.location.replace(config.basePath)
+}
